815336b9e40055bf940e908be826230fbdba7ab6
Jocelyn Naarden <tzionone@gmail.com>
2022-04-28 00:02:39 -0400

    Assignment Completedd⚾

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const p = document.querySelector('#info')
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+
+  })
+    .then(res => res.json())
+    .then(user => {
+      p.textContent = user.id
+    })
+    .catch(err => {
+      p.textContent = err.message
+    })
+}
